1754eba506fbcf73f20a994a2b56d5c14d4fafc4
brajesh708 <brajeshmewada694@gmail.com>
2024-07-22 12:59:45 +0530

    variable

---
+++ short concept/variable.js
@@ -0,0 +1,16 @@
+const account_Id = 144553
+let account_email= "brajeshmewada694@gmail.com"
+var account_pasword= "12345"
+account_city= "sehore"
+let account_state;    //ese undifine variable kahte he
+// account_Id=2
+account_email="raj@gmail.com"
+account_pasword="80976"
+account_city="bhopal"
+console.log(account_Id)
+console.table([account_Id,account_email,account_pasword,account_city,account_state])
+
+
+// run karne ke liye node ke bad me file ke name
+// js me var ka use nahi hota he 
+// esme let or const dono variable ka uuse hota he
