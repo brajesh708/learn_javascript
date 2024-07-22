bbfebdcbf0612f006c6c51773fc74541a29b2acc
brajesh708 <brajeshmewada694@gmail.com>
2024-07-22 12:59:05 +0530

    data type

---
+++ short concept/datatype.js
@@ -0,0 +1,28 @@
+"use strict"; //treal all js code as newer version
+ 
+// alert("hello")  // we are using node.js , not broser
+
+// console.log(2+5)
+// console.log("brajesh")
+
+// let name= "brajesh"
+// let age=20
+// let isLoggenId= false
+
+
+/* prametive data type
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => stanalone value == jab koe  bhi respons na aaye tab null ka use kare
+// undefined =>  variablle bana ke khali cod dena 
+// symbol ==> unique ness ke liye use karte he 
+*/
+console.log(typeof null);
+//  null ek prametive data type he but nikalne par object show karta he ye js ka upbad he
+
+/* object data type
+
+*/
+
