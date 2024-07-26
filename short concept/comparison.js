a463afa0a2e8cdca81f338bbfdb6faad14b0bc48
brajesh708 <brajeshmewada694@gmail.com>
2024-07-27 00:58:50 +0530

    comparision oprator

--- short concept/comparison.js
+++ short concept/comparison.js
@@ -17,9 +17,16 @@
 //  str ko int me outometic convart kar raha he
 
 
-console.log(null>0);
-console.log(null<0);
-console.log(null==0);
-console.log(null!=0);
-console.log(null>=0);
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null==0);
+// console.log(null!=0);  
+// console.log(null>=0);  // true only
+
+
+
+console.log(undefined==0);
+console.log(undefined!=0);   // true
+console.log(undefined>0);   
+console.log(undefined<0);
 
