647e81a396ee474eb5a0cc1a639f5cab7563b89a
brajesh708 <brajeshmewada694@gmail.com>
2024-07-27 00:35:58 +0530

    oprator

--- short concept/oprations.js
+++ short concept/oprations.js
@@ -20,17 +20,18 @@
 // console.log(5**3)
 
 
-let str1 = "hello"
-let str2 = "world"
-let str3 = str1+str2
-console.log(str3)
+// let str1 = "hello"
+// let str2 = "world"
+// let str3 = str1+str2
+// console.log(str3)
 
 // complec  str + number
-console.log("1"+2);
-console.log(1+2);
-console.log(1+2+"3");
-console.log("1"+2+"3");
-console.log(1+2+3);
-console.log(1+2+3+4);
+// console.log("1"+2);
+// console.log(1+2);
+// console.log(1+2+"3");
+// console.log("1"+2+"3");
+// console.log(1+2+3);
+// console.log(1+2+3+4);
 
-
+console.log(+true)  
+console.log(+false)
