802620190be777d894673e21b6ad20293d646571
brajesh708 <brajeshmewada694@gmail.com>
2024-07-27 00:48:07 +0530

    comparison oprator

--- short concept/comparison.js
+++ short concept/comparison.js
@@ -1,7 +1,14 @@
-console.log(2>1);
-console.log(2>=1);
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<!1);
+// console.log(2>!1);
+// console.log(2==1);
+
+console.log("2"<1);
 console.log(2<1);
-console.log(2<!1);
-console.log(2>!1);
-console.log(2==1);
+console.log(2>1);
+console.log("2">1);
+
+
 
