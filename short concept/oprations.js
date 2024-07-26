60b26c3c940c54c8df798ce54e1c51a74f6e755d
brajesh708 <brajeshmewada694@gmail.com>
2024-07-27 00:29:06 +0530

    chenges

--- short concept/oprations.js
+++ short concept/oprations.js
@@ -9,13 +9,18 @@
 // console.log(2/2)
 
 
+// let str1 = "hello"
+// let str2 = "world"
+
+// console.log(str1+str2)
+
+
+// console.log(55+2)
+// console.log(5**2)
+// console.log(5**3)
+
+
 let str1 = "hello"
 let str2 = "world"
-
-console.log(str1+str2)
-
-
-console.log(55+2)
-console.log(5**2)
-console.log(5**3)
-
+let str3 = str1+str2
+console.log(str3)
