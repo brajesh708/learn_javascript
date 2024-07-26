cdc26828627ba67c12389349a843b530c78d320c
brajesh708 <brajeshmewada694@gmail.com>
2024-07-27 00:53:31 +0530

    note

--- short concept/comparison.js
+++ short concept/comparison.js
@@ -1,3 +1,7 @@
+//  esme ans boolean data type == ya too true ya  fir false
+// same data type ko compair  kare 
+// type script alag data type  ko compair karna alow nahi karta he
+
 // console.log(2>1);
 // console.log(2>=1);
 // console.log(2<1);
@@ -5,10 +9,11 @@
 // console.log(2>!1);
 // console.log(2==1);
 
-console.log("2"<1);
-console.log(2<1);
-console.log(2>1);
-console.log("2">1);
+// console.log("2"<1);
+// console.log(2<1);
+// console.log(2>1);
+// console.log("2">1);
 
+//  str ko int me outometic convart kar raha he
 
 
