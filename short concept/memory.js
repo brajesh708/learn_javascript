3138ed3fd8cc5c0ed2c751afa4882eabc1e30cac
brajesh708 <brajeshmewada694@gmail.com>
2024-07-28 11:16:59 +0530

    memory

---
+++ short concept/memory.js
@@ -0,0 +1,18 @@
+// memory 2 type ki hoti he 
+// stack(premitive) , Heap(non- primitive)
+// stack memory ki allocation aur deallocation fast hoti he
+// heap memory ki allocation aur deallocation slow hoti he
+// stack memory ki size fixed hoti he
+// heap memory ki size dynamic hoti he
+// stack memory ki allocation time constant hoti he
+// heap memory ki allocation time variable hoti he
+// stack memory ki deallocation time constant hoti he
+// heap memory ki deallocation time variable hoti he
+// stack memory ki use karna jaruri hota he
+// heap memory ki use karna jaruri nahi hota he
+
+let myname='brajesh'
+anothername='raj rajput'
+
+console.log(myname);
+console.log(anothername);
