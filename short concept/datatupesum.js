82e22306c78f0afc56fe4108c1820e90d46d8983
brajesh708 <brajeshmewada694@gmail.com>
2024-07-28 10:26:33 +0530

    summry

--- short concept/datatupesum.js
+++ short concept/datatupesum.js
@@ -1,3 +1,14 @@
 //  data type ko do bhago me bata gaya he
 // 1 primative
 // 2 non primative
+//        primative data type  ya coll by velue
+// 1. number
+// 2. string
+// 3. boolean
+// 4. null
+// 5. undefined
+// 6. sumbol     7. BigInt 
+//         non primative data type
+// 1. array
+// 2. object
+// 3. function
