aa2d3ebf7f982f8e1a43f3ec896c04629c0b4dee
brajesh708 <brajeshmewada694@gmail.com>
2024-07-30 11:21:43 +0530

    new file

--- short concept/oprations.js
+++ short concept/oprations.js
@@ -23,23 +23,23 @@
 // let str1 = "hello"
 // let str2 = "world"
 // let str3 = str1+str2
-// console.log(str3)
+// console.log(str3)  //hello world
 
 // complec  str + number
-console.log("1"+2); //12
-console.log("5"*2); //10
-console.log("1"-2); //-1
-console.log(1+2); //3
-console.log(1+2+"3"); //33
-console.log("1"+2+"3"); //123
-console.log(1+2+3);//6
-console.log(1+2+3+4); //10
+// console.log("1"+2); //12
+// console.log("5"*2); //10
+// console.log("1"-2); //-1
+// console.log(1+2); //3
+// console.log(1+2+"3"); //33
+// console.log("1"+2+"3"); //123
+// console.log(1+2+3);//6
+// console.log(1+2+3+4); //10
 
 
 
-// console.log(+true)  
-// console.log(true)  
+// console.log(+true)   //1
+// console.log(true)  //true
 
-// console.log(+false)
-// console.log(false)
+// console.log(+false) //0
+// console.log(false) // false
 
