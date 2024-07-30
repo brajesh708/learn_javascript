8643ce71aebf2d6766635097bc6ef455685825bf
brajesh708 <brajeshmewada694@gmail.com>
2024-07-30 11:36:21 +0530

    method of str

--- short concept/strings.js
+++ short concept/strings.js
@@ -1,5 +1,19 @@
-const name= "brajesh"
-const repoCount = 50
+// const name= "brajesh"
+// const repoCount = 50
 
-// console.log(name + repoCount + "value"); //this is od formet
-console.log(`Hello my name is ${name} and my repo cout is ${repoCount}`); // this is new formet
+// // console.log(name + repoCount + "value"); //this is od formet
+// console.log(`Hello my name is ${name} and my repo cout is ${repoCount}`); // this is new formet
+
+const gameNmae = newNmae = "brajesh mewada"
+console.log(gameNmae);
+console.log(gameNmae[1]);
+console.log(gameNmae.__proto__);
+console.log(gameNmae.length);
+console.log(gameNmae.toUpperCase());
+
+
+
+
+
+
+
