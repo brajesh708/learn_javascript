62b79272d960b4e353154d291535f387f1d7bbdb
brajesh708 <brajeshmewada694@gmail.com>
2024-07-30 11:41:49 +0530

    substring

--- short concept/strings.js
+++ short concept/strings.js
@@ -5,11 +5,22 @@
 // console.log(`Hello my name is ${name} and my repo cout is ${repoCount}`); // this is new formet
 
 const gameNmae = newNmae = "brajesh mewada"
-console.log(gameNmae);
-console.log(gameNmae[1]);
-console.log(gameNmae.__proto__);
-console.log(gameNmae.length);
-console.log(gameNmae.toUpperCase());
+// console.log(gameNmae);
+// console.log(gameNmae[1]);
+// console.log(gameNmae.__proto__);
+// console.log(gameNmae.length);
+// console.log(gameNmae.toUpperCase());
+// console.log(gameNmae.charAt(2));
+// console.log(gameNmae.indexOf('h'));
+
+
+
+//  string ke method console.log se jakar pade 
+
+
+const newString = gameNmae.substring(2,5)
+console.log(newString);
+
 
 
 
