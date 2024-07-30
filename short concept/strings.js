0c3ea61413d1a3269835233dab804cf8fc67adb5
brajesh708 <brajeshmewada694@gmail.com>
2024-07-30 11:27:04 +0530

    how to declare

---
+++ short concept/strings.js
@@ -0,0 +1,5 @@
+const name= "brajesh"
+const repoCount = 50
+
+// console.log(name + repoCount + "value"); //this is od formet
+console.log(`Hello my name is ${name} and my repo cout is ${repoCount}`); // this is new formet
