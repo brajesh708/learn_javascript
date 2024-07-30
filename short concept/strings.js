230c606e508171166b7efa97ea09f489493bd890
brajesh708 <brajeshmewada694@gmail.com>
2024-07-30 12:08:45 +0530

    trim method

--- short concept/strings.js
+++ short concept/strings.js
@@ -19,10 +19,18 @@ const gameNmae = newNmae = "brajesh mewada"
 
 
 const newString = gameNmae.substring(2,5)
-const new2String = gameNmae.substring(0,5)
-
 console.log(newString);
+
+const new2String = gameNmae.substring(0,5)
 console.log(new2String);
+
+const anotherString = gameNmae.slice(-8,2)
+console.log(anotherString);
+
+const newStringOne = "   Brajesh  "
+console.log(newStringOne)
+console.log(newStringOne.trim()) // trim funtion space hata deta he
+
 
 
 
