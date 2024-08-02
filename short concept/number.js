c160125dba5efa71cd21940fde7a1a988c5ed285
brajesh708 <brajeshmewada694@gmail.com>
2024-08-02 16:15:10 +0530

    math

--- short concept/number.js
+++ short concept/number.js
@@ -1,4 +1,17 @@
-const score = 300
+// const score = 300
 
-const blance = new Number(100)
-console.log(blance)
+// const blance = new Number(100)
+// console.log(blance)
+
+//  js me number ke under bahut sari method hoti he
+
+
+// ++++++++++++++math++++++++++++++++++++
+
+// console.log(Math);
+
+
+// console.log(Math.abs(-4));  //abs nagative value ko positive karta he
+
+console.log(Math.round(4.3))
+console.log(Math.round(4.8))
