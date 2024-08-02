111fc93b878771b9aef2c4abb8063fa88988ac44
brajesh708 <brajeshmewada694@gmail.com>
2024-08-02 16:24:48 +0530

    random

--- short concept/number.js
+++ short concept/number.js
@@ -13,5 +13,18 @@
 
 // console.log(Math.abs(-4));  //abs nagative value ko positive karta he
 
-console.log(Math.round(4.3))
-console.log(Math.round(4.8))
+// console.log(Math.round(4.3))
+// console.log(Math.round(4.8)) // round aprosimate value deta he
+
+
+// console.log(Math.ceil(4.2));  // ceil hamesa top velue hi deta he
+// console.log(Math.floor(4.9));  // ceil hamesa doun velue hi deta he
+
+
+// console.log(Math.min(2,3,4,5,6));
+// console.log(Math.max(2,3,4,5,6));
+
+// console.log(Math.random()); // math.random me hamesa value 0 se 1 ke bich me hi aati he 
+
+// console.log(Math.random()*100); // math.random me hamesa value 0 se 1
+// console.log(Math.random()*100+1); // math.random me hamesa value 0 se badane ke liye 1 + kar dete he
