d60ac8207e45b71b38d8c3f32d24d8ee5ad4670b
brajesh708 <brajeshmewada694@gmail.com>
2024-08-07 21:33:41 +0530

    new folder

---
+++ part2/array.js
@@ -0,0 +1,8 @@
+//array
+// JavaScript arrays are resizable and can contain a mix of different data types. (When those characteristics are undesirable, use typed arrays instead.)
+// JavaScript arrays are not associative arrays and so, array elements cannot be accessed using arbitrary strings as indexes, but must be accessed using nonnegative integers (or their respective string form) as indexes.
+// JavaScript arrays are zero-indexed: the first element of an array is at index 0, the second is at index 1, and so on — and the last element is at the value of the array's length property minus 1.
+// JavaScript array-copy operations create shallow copies. (All standard built-in copy operations with any JavaScript objects create shallow copies, rather than deep copies).  
+
+const myArry = [0,1,2,3,4,5]
+console.log(myArry.length) //6
