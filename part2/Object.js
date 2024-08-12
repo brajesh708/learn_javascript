93dec19794798bc2307f56450db6c11ee187ee97
brajesh708 <brajeshmewada694@gmail.com>
2024-08-12 08:47:19 +0530

    obj 1

--- part2/Object.js
+++ part2/Object.js
@@ -4,9 +4,16 @@
 // object literals
 const Jsuser = {
     name : "brajesh",
+    fullName: "brajesh mewada",
     age : 20,
     email : "brajesh@gmail.com",
     isLoggedIn : false
 }
 
 console.log(Jsuser);
+console.log(Jsuser.age);
+console.log(Jsuser["age"]);
+// console.log(Jsuser."full name"); // error
+console.log(Jsuser["fullName"]); // output brajesh mewada
+
+
