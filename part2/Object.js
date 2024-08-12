6bb830dc53a940caea7b16a98d7f40b73442b148
brajesh708 <brajeshmewada694@gmail.com>
2024-08-12 08:53:16 +0530

    symbol

--- part2/Object.js
+++ part2/Object.js
@@ -2,18 +2,20 @@
 // object.create
 
 // object literals
-const Jsuser = {
-    name : "brajesh",
-    fullName: "brajesh mewada",
-    age : 20,
-    email : "brajesh@gmail.com",
-    isLoggedIn : false
-}
+// const Jsuser = {
+//     name : "brajesh",
+//     fullName: "brajesh mewada",
+//     age : 20,
+//     email : "brajesh@gmail.com",
+//     isLoggedIn : false
+// }
 
-console.log(Jsuser);
-console.log(Jsuser.age);
-console.log(Jsuser["age"]);
-// console.log(Jsuser."full name"); // error
-console.log(Jsuser["fullName"]); // output brajesh mewada
+// console.log(Jsuser);
+// console.log(Jsuser.age);
+// console.log(Jsuser["age"]);
+// // console.log(Jsuser."full name"); // error
+// console.log(Jsuser["fullName"]); // output brajesh mewada
 
-
+//  how to define symbol in js
+const sym = Symbol("brajesh");
+console.log(sym);
