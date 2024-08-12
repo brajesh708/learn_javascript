1293113a0e3d3152041203bc4002455f84ec0fce
brajesh708 <brajeshmewada694@gmail.com>
2024-08-12 08:41:06 +0530

    new obj

--- part2/Object.js
+++ part2/Object.js
@@ -1,1 +1,12 @@
 // singalton object hota  he
+// object.create
+
+// object literals
+const Jsuser = {
+    name : "brajesh",
+    age : 20,
+    email : "brajesh@gmail.com",
+    isLoggedIn : false
+}
+
+console.log(Jsuser);
