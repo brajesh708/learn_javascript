c7c01d31d4fb62fc071a5007cf55002cff7981d7
brajesh708 <brajeshmewada694@gmail.com>
2024-08-20 15:52:16 +0530

    new data

---
+++ part2/object2.js
@@ -0,0 +1,7 @@
+//  singalton object --> canstatior se bana too singal tan hoga bas
+// Object.create   ==> this is singal ton way of object
+
+// object literals
+
+const Jsuser = {}
+Jsuser.name = 'Rahul'
