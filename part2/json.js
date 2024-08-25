c294d27ce906e8ddac0af70eeb880c0dfef4dd8d
brajesh708 <brajeshmewada694@gmail.com>
2024-08-25 13:31:02 +0530

    json formate

---
+++ part2/json.js
@@ -0,0 +1,10 @@
+const course = {
+    name: "Full Stack Development",
+    duration: "12 weeks",
+    price: 12000,
+    instructor: "John Doe",
+}
+
+console.log(course.name)
+console.log(course["duration"])
+
