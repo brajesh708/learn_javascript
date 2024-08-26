308f44a0853e516ab46c9fc0647542682bdee61c
brajesh708 <brajeshmewada694@gmail.com>
2024-08-26 13:20:55 +0530

    new

--- part3/loop.js
+++ part3/loop.js
@@ -1,12 +1,28 @@
 //for
 
-for (let index = 0; index < 10; index++) {
-    const element = index;
-    console.log(element)
-}
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element)
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     console.log(element)
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5){
+//         console.log("5 is best numwer")
+//     }
+//     console.log(element)
+// }
+
 
 for (let i = 0; i <= 10; i++) {
     const element = i;
+    if (element == 5){
+        console.log("5 is best numwer")
+    }
     console.log(element)
 }
-
