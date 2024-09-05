4e16727d6f9e5f48ec9869919c549c6f9665d8ca
brajesh708 <brajeshmewada694@gmail.com>
2024-09-05 11:40:56 +0530

    addnumber

--- part4/function.js
+++ part4/function.js
@@ -1,26 +1,35 @@
-console.log("B");
-console.log("R");
-console.log("A");
-console.log("J");
-console.log("E");
-console.log("S");
-console.log("H");
+// console.log("B");
+// console.log("R");
+// console.log("A");
+// console.log("J");
+// console.log("E");
+// console.log("S");
+// console.log("H");
 
 
-function sayMyName(){
-    console.log("My name is JESHAH");
+// function sayMyName(){
+//     console.log("My name is JESHAH");
+    
+// }
+// sayMyName()
+
+// function myName(){
+//  console.log("B");
+// console.log("R");
+// console.log("A");
+// console.log("J");
+// console.log("E");
+// console.log("S");
+// console.log("H");
+
+// }
+// myName()
+
+
+function addTwoNumber(){
+    let num1 = 5;
+    let num2 = 10;
+    console.log(num1+num2);
     
 }
-sayMyName()
-
-function myName(){
- console.log("B");
-console.log("R");
-console.log("A");
-console.log("J");
-console.log("E");
-console.log("S");
-console.log("H");
-
-}
-myName()
+addTwoNumber()
