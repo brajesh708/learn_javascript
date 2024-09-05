222737e7a157240d5262768365dd98bc454626a9
brajesh708 <brajeshmewada694@gmail.com>
2024-09-05 11:33:28 +0530

    new functions

--- part4/function.js
+++ part4/function.js
@@ -7,4 +7,20 @@ console.log("S");
 console.log("H");
 
 
+function sayMyName(){
+    console.log("My name is JESHAH");
+    
+}
+sayMyName()
 
+function myName(){
+ console.log("B");
+console.log("R");
+console.log("A");
+console.log("J");
+console.log("E");
+console.log("S");
+console.log("H");
+
+}
+myName()
