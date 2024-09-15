6b56002444f41742ce6cb2d1dac23f3b81700360
brajesh708 <brajeshmewada694@gmail.com>
2024-09-15 09:44:04 +0530

    new

--- part2/json.js
+++ part2/json.js
@@ -1,11 +1,20 @@
+// const course = {
+//     name: "Full Stack Development",
+//     duration: "12 weeks",
+//     price: 12000,
+//     instructor: "John Doe",
+// }
+
+// console.log(course.name)
+// console.log(course["duration"])
+
+
 const course = {
     name: "Full Stack Development",
     duration: "12 weeks",
-    price: 12000,
+    price: 1200,
     instructor: "John Doe",
 }
 
 console.log(course.name)
 console.log(course["duration"])
-
-
