c2594a5563baf573d566d06449b5b17ed6bad9c6
brajesh708 <brajeshmewada694@gmail.com>
2024-10-19 21:33:51 +0530

    update

--- part4/function.js
+++ part4/function.js
@@ -10,20 +10,20 @@ console.log("H");
 function sayMyName(){
     console.log("My name is JESHAH");
     
-// }
-// sayMyName()
+}
+sayMyName()
 
-// function myName(){
-//  console.log("B");
-// console.log("R");
-// console.log("A");
-// console.log("J");
-// console.log("E");
-// console.log("S");
-// console.log("H");
+function myName(){
+ console.log("B");
+console.log("R");
+console.log("A");
+console.log("J");
+console.log("E");
+console.log("S");
+console.log("H");
 
-// }
-// myName()
+}
+myName()
 
 
 function addTwoNumber(){
