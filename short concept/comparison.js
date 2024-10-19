9516bbefe0140c52ec52833512a992d2118cb043
brajesh708 <brajeshmewada694@gmail.com>
2024-10-19 21:32:07 +0530

    update

--- short concept/comparison.js
+++ short concept/comparison.js
@@ -9,26 +9,26 @@ console.log(2<!1);
 console.log(2>!1);
 console.log(2==1);
 
-// console.log("2"<1);
-// console.log(2<1);
-// console.log(2>1);
-// console.log("2">1);
+console.log("2"<1);
+console.log(2<1);
+console.log(2>1);
+console.log("2">1);
 
 //  str ko int me outometic convart kar raha he
 
 //  note == esse niche bale  code ko aboid karte he
-// console.log(null>0);
-// console.log(null<0);
-// console.log(null==0);
-// console.log(null!=0);  
-// console.log(null>=0);  // true only
+console.log(null>0);
+console.log(null<0);
+console.log(null==0);
+console.log(null!=0);  
+console.log(null>=0);  // true only
 
 
 
-// console.log(undefined==0);
-// console.log(undefined!=0);   // true
-// console.log(undefined>0);   
-// console.log(undefined<0);
+console.log(undefined==0);
+console.log(undefined!=0);   // true
+console.log(undefined>0);   
+console.log(undefined<0);
 
 console.log("2"===2);  //false
 console.log("2"==2);  //true
