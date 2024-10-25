b1e6f2c8f58b1372320ae397a43ccec842ebec86
brajesh708 <brajeshmewada694@gmail.com>
2024-10-25 20:57:00 +0530

    update

--- short concept/variable.js
+++ short concept/variable.js
@@ -1,14 +1,14 @@
-// const account_Id = 144553
-// let account_email= "brajeshmewada694@gmail.com"
-// var account_pasword= "12345"
-// account_city= "sehore"
-// let account_state;    //ese undifine variable kahte he
-// // account_Id=2
-// account_email="raj@gmail.com"
-// account_pasword="80976"
-// account_city="bhopal"
-// console.log(account_Id)
-// console.table([account_Id,account_email,account_pasword,account_city,account_state])
+const account_Id = 144553
+let account_email= "brajeshmewada694@gmail.com"
+var account_pasword= "12345"
+account_city= "sehore"
+let account_state;    //ese undifine variable kahte he
+// account_Id=2
+account_email="raj@gmail.com"
+account_pasword="80976"
+account_city="bhopal"
+console.log(account_Id)
+console.table([account_Id,account_email,account_pasword,account_city,account_state])
 
 
 // run karne ke liye node ke bad me file ke name
